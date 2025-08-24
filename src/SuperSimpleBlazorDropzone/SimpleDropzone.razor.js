cde3433e7ae954863dddafb4cbf2e1600f4a4475
Nicola Piermattei <pierma.nick@gmail.com>
2025-08-24 08:29:19 +0200

    Cleaned up JS code

--- src/SuperSimpleBlazorDropzone/SimpleDropzone.razor.js
+++ src/SuperSimpleBlazorDropzone/SimpleDropzone.razor.js
@@ -5,42 +5,28 @@ export const setDragActive = (elementId, active, cssClass) => {
         else dropper.classList.remove(cssClass);
     }
 }
-export const handleDrop = (dotNetRef, event, contentOutputType) => {
+export const handleDrop = async (dotNetRef, event) => {
     event.preventDefault();
     setDragActive(false);
     const files = event.dataTransfer.files;
     if (files.length > 0) {
         const file = files[0];
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            const data = {
-                name: file.name,
-                extension: file.name.split('.').pop(),
-                type: file.type,
-                size: file.size
-            };
-            if (contentOutputType === 'Base64') {
-                data.content = e.target.result.split('base64,')[1];
-                dotNetRef.invokeMethodAsync('ReceiveBase64Data', data);
-            }
-            if (contentOutputType === 'Binary' && file) {
-                data.content = new Uint8Array(e.target.result);
-                dotNetRef.invokeMethodAsync('ReceiveBinaryData', data);
-            }
+        const blob = new Uint8Array(await file.arrayBuffer());
+        const data = {
+            name: file.name,
+            extension: file.name.split('.').pop(),
+            type: file.type,
+            size: file.size,
+            content: blob
         };
-        if (contentOutputType === 'Binary') {
-            reader.readAsArrayBuffer(file);
-        }
-        if (contentOutputType === 'Base64') {
-            reader.readAsDataURL(file);
-        }
+        dotNetRef.invokeMethodAsync('ReceiveBinaryData', data);
     }
 }
-export const registerDropHandler = (dotNetRef, elementId, contentOutputType) => {
-    document.getElementById(elementId)?.addEventListener('drop', dropEventListener(dotNetRef, contentOutputType));
+export const registerDropHandler = (dotNetRef, elementId) => {
+    document.getElementById(elementId)?.addEventListener('drop', dropEventListener(dotNetRef));
 }
-export const removeEventListeners = (dotNetRef, elementId, contentOutputType) => {
-    document.getElementById(elementId)?.removeEventListener('drop', dropEventListener(dotNetRef, contentOutputType));
+export const removeEventListeners = (dotNetRef, elementId) => {
+    document.getElementById(elementId)?.removeEventListener('drop', dropEventListener(dotNetRef));
 }
 export const clickHiddenFileInput = (fileInputId) => {
     const input = document.getElementById(fileInputId);
@@ -50,5 +36,5 @@ export const clickHiddenFileInput = (fileInputId) => {
         input.dispatchEvent(evt);
     }
 }
-export const dropEventListener = (dotNetRef, contentOutputType) => 
-    (e) => handleDrop(dotNetRef, e, contentOutputType);
+export const dropEventListener = (dotNetRef) => 
+    (e) => handleDrop(dotNetRef, e);
